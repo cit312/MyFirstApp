51121c8b4dc0110d30b4d618fffcc6fda7950e55
brayden hancock <brayden.hancock@tvw.org>
2013-07-04 01:44:06 -0700

    Added some comments. Collects hashmap correctly

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -29,23 +29,35 @@ public class MainActivity extends Activity {
 	
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
+		//For Debugging
 		System.out.println("sendMessage...");
+		
+		//Debugging: create data to send to server
 		HashMap<String,String> numbers = new HashMap<String,String>();
-		numbers.put("one", "more");
+		numbers.put("userID", "16");
 		numbers.put("two", "another");
 		numbers.put("three", "The third thing");
 		
-		CommBean data = new CommBean("Testing");
+		//Put data into CommBean (the command as String and data as hashmap)
+		CommBean data = new CommBean("getPivots");
 		data.setData(numbers);
 		
+		//Start the socket
 		Client c = new Client(data);
 		HashMap returnedData = c.transmit();
 		
+		//Get returned data
+		returnedData = (HashMap) returnedData.get("data");
 		
+		//Debugging
+		System.out.println("--THE RETURNED DATA --");
+		System.out.println(returnedData);
 		
+		//Build and start intent
 	    Intent intent = new Intent(this, DisplayMessageActivity.class);
 	    EditText editText = (EditText) findViewById(R.id.edit_message);
 	    String message = editText.getText().toString();
+	    //String message = (String) returnedData.get("Pivot 1");
 	    intent.putExtra(EXTRA_MESSAGE, message);
 	    startActivity(intent);
 	}
