5ba6731d941f9f7a48f598d57b1ee7097e4f9858
brayden hancock <brayden.hancock@tvw.org>
2013-07-04 02:48:27 -0700

    Loops through pivots

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.myfirstapp;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 
 import android.os.Bundle;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +25,60 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//For Debugging
+		System.out.println("sendMessage...");
+				
+		//Debugging: create data to send to server
+		HashMap<String,String> numbers = new HashMap<String,String>();
+		numbers.put("userID", "16");
+		numbers.put("two", "another");
+		numbers.put("three", "The third thing");
+				
+		//Put data into CommBean (the command as String and data as hashmap)
+		CommBean data = new CommBean("getPivots");
+		data.setData(numbers);
+				
+		//Start the socket
+		Client c = new Client(data);
+		HashMap returnedData = c.transmit();
+		
+		//Check if error
+		
+		//Get returned data
+		returnedData = (HashMap) returnedData.get("data");
+		
+		//The loop
+		for (String key : new HashSet<String>(returnedData.keySet())) {
+			System.out.println(((HashMap)returnedData.get(key)).get("Attr3"));
+		}
+		
+		
+		HashMap firstPivot = (HashMap) returnedData.get("Pivot 1");
+		HashMap secondPivot = (HashMap) returnedData.get("Pivot 2");
+		
+				
+		//Debugging
+		System.out.println(returnedData.keySet());
+		System.out.println("--THE RETURNED DATA --");
+		System.out.println(returnedData);
+		
+	    String message = (String) firstPivot.get("Attr3");
+	    String message2 = (String) secondPivot.get("Attr3");
+
+        
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.root);
+        TextView txt1 = new TextView(this);
+        txt1.setTextSize(40);
+        txt1.setText(message);
+        TextView txt2 = new TextView(this);
+        txt2.setTextSize(40);
+        txt2.setText(message2);
+        
+        //linearLayout.setBackgroundColor(Color.TRANSPARENT);
+        linearLayout.addView(txt1);
+        linearLayout.addView(txt2);
+        
 	}
 
 	@Override
@@ -29,35 +90,10 @@ public class MainActivity extends Activity {
 	
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
-		//For Debugging
-		System.out.println("sendMessage...");
-		
-		//Debugging: create data to send to server
-		HashMap<String,String> numbers = new HashMap<String,String>();
-		numbers.put("userID", "16");
-		numbers.put("two", "another");
-		numbers.put("three", "The third thing");
-		
-		//Put data into CommBean (the command as String and data as hashmap)
-		CommBean data = new CommBean("getPivots");
-		data.setData(numbers);
-		
-		//Start the socket
-		Client c = new Client(data);
-		HashMap returnedData = c.transmit();
-		
-		//Get returned data
-		returnedData = (HashMap) returnedData.get("data");
-		
-		//Debugging
-		System.out.println("--THE RETURNED DATA --");
-		System.out.println(returnedData);
-		
 		//Build and start intent
 	    Intent intent = new Intent(this, DisplayMessageActivity.class);
 	    EditText editText = (EditText) findViewById(R.id.edit_message);
 	    String message = editText.getText().toString();
-	    //String message = (String) returnedData.get("Pivot 1");
 	    intent.putExtra(EXTRA_MESSAGE, message);
 	    startActivity(intent);
 	}
